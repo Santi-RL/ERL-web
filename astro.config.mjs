bb90f9633603a66060a00d0b88d3892260ec025b
Santi-RL <rojaslem@gmail.com>
2025-06-11 22:06:13 -0300

    Agrego site base para deploy en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://santi-rl.github.io/ERL-web/',
   vite: {
     plugins: [tailwindcss()]
   }
